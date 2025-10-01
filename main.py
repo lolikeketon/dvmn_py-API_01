b721dc6f46978fa721a410a358b9e21bf4cc8dd4
lolikeketon <hahawqwqofficial@gmail.com>
2025-10-01 20:53:35 +0500

    Внёс изменения в основной код
    - Переименовал функцию с добавлением глагола
    
    - Вынес лишний цикл в функцию main()
    
    - Добавил словарь с параметрами для GET запроса

--- main.py
+++ main.py
@@ -1,22 +1,27 @@
 import requests
 
 
-def terminal_weather(locations):
+def get_terminal_weather(location):
     url_template = 'https://wttr.in{}'
+    payload = {'lang': 'ru',
+               'M': '',
+               'n': '',
+               'q': '',
+               'T': ''}
 
-    for location in locations:
-        url = url_template.format('/' + location + '?lang=ru&M&n&q&T')
+    url = url_template.format('/' + location)
 
-        response = requests.get(url)
-        response.raise_for_status()
+    response = requests.get(url, params=payload)
+    response.raise_for_status()
 
-        print(response.text)
+    return response.text
 
 
 def main():
     places = ['Лондон', 'svo', 'Череповец']
 
-    terminal_weather(places)
+    for place in places:
+        print(get_terminal_weather(place))
 
 
 if __name__ == '__main__':
